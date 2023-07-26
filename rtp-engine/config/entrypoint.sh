068c151a685c63ab4745d8894f84039c9d8719d8
vishalk17 <vishalkapadi95@gmail.com>
2023-07-26 17:45:11 +0530

    Revert "rtpengine: mic changes"
    This reverts commit 2ba877ad21b1e43fb4404ce67e027c6ff41955e4.

--- rtp-engine/config/entrypoint.sh
+++ rtp-engine/config/entrypoint.sh
@@ -3,40 +3,38 @@ set -e
 
 PATH=/usr/local/bin:$PATH
 
-CLOUD=
-
 case $CLOUD in 
-  gcp)
-    LOCAL_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
-    PUBLIC_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-    ;;
-  aws)
-    LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-    PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-    ;;
-  digitalocean)
-    LOCAL_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
-    PUBLIC_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-    ;;
-  azure)
-    LOCAL_IP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text")
-    PUBLIC_IP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text")
-    ;;
-  *)
-    ;;
+  gcp)
+    LOCAL_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+    PUBLIC_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+    ;;
+  aws)
+    LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+    PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+    ;;
+  digitalocean)
+    LOCAL_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
+    PUBLIC_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+    ;;
+  azure)
+    LOCAL_IP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text")
+    PUBLIC_IP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-08-01&format=text")
+    ;;
+  *)
+    ;;
 esac
 
 if [ -n "$PUBLIC_IP" ]; then
-  MY_IP="$LOCAL_IP"!"$PUBLIC_IP"
+  MY_IP="$LOCAL_IP"!"$PUBLIC_IP"
 else
-  MY_IP=$(hostname -I | awk '{print $1}')
+  MY_IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 fi
 
 #sed -i -e "s/MY_IP/$MY_IP/g" /etc/rtpengine.conf
 
 if [ "$1" = 'rtpengine' ]; then
-  shift
-  exec rtpengine --config-file /etc/rtpengine.conf  "$@"
+  shift
+  exec rtpengine --config-file /etc/rtpengine.conf  "$@"
 fi
 
 exec "$@"
