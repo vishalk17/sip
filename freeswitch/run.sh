6892b8e26cc78e9f0dbdf83286008e11a7824bb8
vishalk17 <vishalkapadi95@gmail.com>
2025-01-24 20:09:21 +0000

    freeswitch : fix typos & identations

--- freeswitch/run.sh
+++ freeswitch/run.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
+set -e
 
-
-set -xe
-
+# Configure FreeSWITCH
 CFG_PATH="/usr/local/freeswitch/conf/vars.xml"
 
-FS_IP="localhost"
-FS_PORT="5060"
-GLOBAL_CODEC_FS="OPUS,G722,PCMU,PCMA,H264,VP8"
-OUTBOUND_CODEC_FS="OPUS,G722,PCMU,PCMA,H264,VP8"
+# Default values
+: ${FS_IP:="localhost"}
+: ${FS_PORT:="5060"}
+: ${GLOBAL_CODEC_FS:="OPUS,G722,PCMU,PCMA,H264,VP8"}
+: ${OUTBOUND_CODEC_FS:="OPUS,G722,PCMU,PCMA,H264,VP8"}
 
-
+# Update configuration
 sed -i "s/FS_IP/$FS_IP/g" $CFG_PATH
 sed -i "s/FS_PORT/$FS_PORT/g" $CFG_PATH
 sed -i "s/GLOBAL_CODEC_FS/$GLOBAL_CODEC_FS/g" $CFG_PATH
 sed -i "s/OUTBOUND_CODEC_FS/$OUTBOUND_CODEC_FS/g" $CFG_PATH
 
+# Start FreeSWITCH
 /usr/local/freeswitch/bin/freeswitch
