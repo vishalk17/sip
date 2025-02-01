ea8c0e32869cac3b26bcb91b8729a12a5089aa3f
vishalk17 <vishalkapadi95@gmail.com>
2025-02-01 13:13:34 +0530

    init.sql : fix db already exists issue
    Errors were.
    
    postgresql  | 2025-02-01 06:48:47.071 UTC [62] ERROR:  database "freeswitch" already exists
    postgresql  | 2025-02-01 06:48:47.071 UTC [62] STATEMENT:  CREATE DATABASE freeswitch OWNER freeswitch;
    postgresql  | psql:/docker-entrypoint-initdb.d/init.sql:2: ERROR:  database "freeswitch" already exists
    postgresql exited with code 3
    dependency failed to start: container for service "postgresql" exited (3)

--- freeswitch/init.sql
+++ freeswitch/init.sql
@@ -1,3 +1,20 @@
-CREATE USER freeswitch WITH PASSWORD 'freeswitch@vishalk17';
-CREATE DATABASE freeswitch OWNER freeswitch;
+-- Create database if it doesn't exist
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'freeswitch') THEN
+        CREATE DATABASE freeswitch;
+    END IF;
+END
+$$;
+
+-- Create user if it doesn't exist
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'freeswitch') THEN
+        CREATE USER freeswitch WITH PASSWORD 'freeswitch@vishalk17';
+    END IF;
+END
+$$;
+
+-- Grant privileges (this is idempotent)
 GRANT ALL PRIVILEGES ON DATABASE freeswitch TO freeswitch;
